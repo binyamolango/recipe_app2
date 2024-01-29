95cd4a3f27e78ccee7cbbdebcc1ef82a8b92d158
binyamyohannes <olangobinyam@gmail.com>
2024-01-29 20:59:17 +0300

    Update login

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-  
-  has_many :recipes
-end
